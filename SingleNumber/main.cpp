24b9fbbd1d6e2b439392d075f93c4682649963ec
DUC DO <duc1023as@gmail.com>
2023-08-21 18:17:09 +0700

    SingleNumber

--- SingleNumber/main.cpp
+++ SingleNumber/main.cpp
@@ -4,15 +4,6 @@
 #include<algorithm>
 
 int singleNumber(std::vector<int>& nums){
-    // std::unordered_map<int,bool> myMap;
-    // for(int i : nums){
-    //     myMap.insert(std::make_pair(i,true));
-    // }
-    // for(std::unordered_map<int,bool>::iterator it=myMap.begin();it!= myMap.end();it++){
-    //     if(std::count(nums.begin(),nums.end(),(*it).first) == 1){
-    //         return (*it).first;
-    //     }
-    // }
     std::unordered_map<int,int> myMap;
     for(int i:nums){
         myMap[i]++;
