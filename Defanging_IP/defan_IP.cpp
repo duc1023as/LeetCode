841757996edb1a21c2fdd2a845ff01cab2ef8fdb
DUC DO <duc1023as@gmail.com>
2023-08-21 16:40:22 +0700

    Happy number

--- Defanging_IP/defan_IP.cpp
+++ Defanging_IP/defan_IP.cpp
@@ -5,7 +5,6 @@ std::string defangIPaddr(std::string address) {
     for(int i=0;i<address.size();i++){
         if(address[i] == '.'){
             address.insert(i,"[.]");
-            std::cout<<"i: "<<i<<std::endl;
             address.erase(i+3,1);
             i+=3;
         }
